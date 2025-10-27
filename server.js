63ed922c2b703c4f3c17d144a5df34e535e7cf54
Ole Kodehode <ole.kodehode@gmail.com>
2025-10-27 13:18:01 +0100

    small change that did nothing

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // Middleware
+app.use(logger);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(logger);
 app.use(cookieParser);
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(cors(corsOptions));
